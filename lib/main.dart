9f7f3a4f501330ab7d076d8a12da9a0f46c19c2f
Seobs <subi88@naver.com>
2019-04-20 15:05:13 +0900

    Add tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Home());
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.directions_car)),
+                    Tab(icon: Icon(Icons.directions_transit)),
+                    Tab(icon: Icon(Icons.directions_bike)),
+                  ],
+                ),
+                title: Text('Tabs Demo'),
+              ),
+              body: TabBarView(
+                  children: [
+                    Icon(Icons.directions_car),
+                    Icon(Icons.directions_car),
+                    Icon(Icons.directions_car),
+                  ],
+              ),
+          ),
+        ),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
