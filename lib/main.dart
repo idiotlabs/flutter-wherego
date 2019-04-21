8b4ef979ce2260b3e9fa26448d46f07a58b37c21
Seobs <subi88@naver.com>
2019-04-21 15:18:27 +0900

    Change Tab icon

--- lib/main.dart
+++ lib/main.dart
@@ -29,18 +29,18 @@ class Home extends StatelessWidget {
               appBar: AppBar(
                 bottom: TabBar(
                   tabs: [
-                    Tab(icon: Icon(Icons.directions_car)),
-                    Tab(icon: Icon(Icons.directions_transit)),
-                    Tab(icon: Icon(Icons.directions_bike)),
+                    Tab(icon: Icon(Icons.view_list)),
+                    Tab(icon: Icon(Icons.person)),
+                    Tab(icon: Icon(Icons.bookmark)),
                   ],
                 ),
                 title: Text('Tabs Demo'),
               ),
               body: TabBarView(
                   children: [
-                    Icon(Icons.directions_car),
-                    Icon(Icons.directions_car),
-                    Icon(Icons.directions_car),
+                    Icon(Icons.view_list),
+                    Icon(Icons.person),
+                    Icon(Icons.bookmark),
                   ],
               ),
           ),
